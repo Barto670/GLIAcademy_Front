57726cc65b26320de9cb549ce787d572be8fa0ff
bartek.skorecki <59570853+Barto670@users.noreply.github.com>
2022-03-02 21:56:43 +0000

    -including correct and incorrect quiz answers and display

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -7,45 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  nextButton = false;
-  checkAnswer = true;
 
+  //disabling and enabling buttons
+  questionClickedDisabled = true;
+  questionCheckedAnswer = false;
+
+  //after a question is answered
   correctAnswer = false;
   wrongAnswer = false;
+
+  //the question we answer
+  questionAnswered : number = 0;
+
+  //the correct answer number
+  correctAnswerNumber : number = 3;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  turnOnNextButton(){
-    this.nextButton = true;
-    this.checkAnswer = false;
-    var element = <HTMLInputElement> document.getElementById("nextButton");
-    element.disabled = false;
+  //when the user clicks on any answer we turn the button on
+  turnOnCheckAnswerButton(){
+    this.questionClickedDisabled = false;
   }
 
-  turnOnCheckAnswerButton(){
-    this.checkAnswer = true;
-    this.nextButton = false;
-    var element = <HTMLInputElement> document.getElementById("checkAnswer");
-    element.disabled = false;
+  //when the user clicks on 'check answer'
+  turnOnNextButton(){
+    this.questionCheckedAnswer = true;
+    
+     //all buttons off and disable them
+     let elements:any = <HTMLInputElement> document.getElementById("1");
+     elements.setAttribute("selected", "false");
+     elements.setAttribute("disabled", "true");
+     elements = <HTMLInputElement> document.getElementById("2");
+     elements.setAttribute("selected", "false");
+     elements.setAttribute("disabled", "true");
+     elements = <HTMLInputElement> document.getElementById("3");
+     elements.setAttribute("selected", "false");
+     elements.setAttribute("disabled", "true");
+     elements = <HTMLInputElement> document.getElementById("4");
+     elements.setAttribute("selected", "false");
+     elements.setAttribute("disabled", "true");
+
+     //check if user answered correctly
+     if(this.questionAnswered == this.correctAnswerNumber){
+        this.correctAnswer = true;
+     }else{
+        this.wrongAnswer = true;
+     }
+
+     //highlight the correct answer
+     elements = <HTMLInputElement> document.getElementById(this.correctAnswerNumber.toString());
+     elements.setAttribute("selected", "true");
+     
   }
+
+  
 
   quizClicked(question: any){
-    
+    this.questionAnswered = question;
     this.turnOnCheckAnswerButton();
     //button clicked
     let element:any = <HTMLInputElement> document.getElementById(question);
 
     //all buttons off
-    let elements:any = <HTMLInputElement> document.getElementById("answer1");
+    let elements:any = <HTMLInputElement> document.getElementById("1");
     elements.setAttribute("selected", "false");
-    elements = <HTMLInputElement> document.getElementById("answer2");
+    elements = <HTMLInputElement> document.getElementById("2");
     elements.setAttribute("selected", "false");
-    elements = <HTMLInputElement> document.getElementById("answer3");
+    elements = <HTMLInputElement> document.getElementById("3");
     elements.setAttribute("selected", "false");
-    elements = <HTMLInputElement> document.getElementById("answer4");
+    elements = <HTMLInputElement> document.getElementById("4");
     elements.setAttribute("selected", "false");
 
     //clicked button on
@@ -54,7 +87,40 @@ export class QuizComponent implements OnInit {
     console.log(element)
   }
 
-  checkUserAnswer(){
+  onNextButtonClicked(){
+
+    //switch to next question
+
+
+
+
+    //disabling and enabling buttons
+    this.questionClickedDisabled = true;
+    this.questionCheckedAnswer = false;
+
+    //after a question is answered
+    this.correctAnswer = false;
+    this.wrongAnswer = false;
+
+    //the question we answer
+    this.questionAnswered = 0;
+
+    //the correct answer number
+    this.correctAnswerNumber = 3;
+
+    //all buttons off and disable them
+    let elements:any = <HTMLInputElement> document.getElementById("1");
+    elements.setAttribute("selected", "false");
+    elements.setAttribute("disabled", "false");
+    elements = <HTMLInputElement> document.getElementById("2");
+    elements.setAttribute("selected", "false");
+    elements.setAttribute("disabled", "false");
+    elements = <HTMLInputElement> document.getElementById("3");
+    elements.setAttribute("selected", "false");
+    elements.setAttribute("disabled", "false");
+    elements = <HTMLInputElement> document.getElementById("4");
+    elements.setAttribute("selected", "false");
+    elements.setAttribute("disabled", "false");
 
   }
 
