bd5e12d0081ea7680aada0f3caec690c77f3a5c4
bartek.skorecki <59570853+Barto670@users.noreply.github.com>
2022-03-23 17:39:22 +0000

    -updated coding section

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,13 +1,13 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import * as ace from 'ace-builds';
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-github';
 import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/ext-beautify';
 
 const LANG = 'ace/mode/html';
-
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
@@ -16,35 +16,144 @@ const LANG = 'ace/mode/html';
 export class ContactUsComponent implements OnInit {
 
   @ViewChild('editor') private editor!: ElementRef<HTMLElement>;
+  @Input() content!: string;
   aceEditor!: ace.Ace.Editor;
+  editorBeautify : any;
+
+  htmltest :any;
+  
 
     constructor() { }
 
   ngOnInit(): void {
     ace.require('ace/ext/language_tools');
+    this.editorBeautify = ace.require('ace/ext/beautify');
   }
 
   
 
   ngAfterViewInit(): void {
-    this.aceEditor = ace.edit(this.editor.nativeElement);
-     ace.config.set("fontSize", "14px");
+    
+    const editorOptions = this.getEditorOptions(); //get options
+
+    this.aceEditor = ace.edit(this.editor.nativeElement,editorOptions); //initialise the editor
+    
+    //add config
+    //  ace.config.set("fontSize", "14px");
      ace.config.set(
         'basePath',
         "https://ace.c9.io/build/src-noconflict/"
      );
 
      
-     this.aceEditor.setTheme("ace/theme/monokai");
-     this.aceEditor.session.setMode("ace/mode/html");
+     this.aceEditor.setTheme("ace/theme/monokai"); //set theme
+     this.aceEditor.session.setMode("ace/mode/html");  //set language
+
+     //enable prints to console
      this.aceEditor.on("change", () => {
         console.log(this.aceEditor.getValue());
      });
 
+    //  this.setContent(this.content || INIT_CONTENT);
 
-     this.aceEditor.getSession().setMode(LANG);
+    // this.aceEditor.setValue("hello",-1);
+    // this.aceEditor.setValue("hello222",1);
+
+    
+
+     this.setCodeTutorial();
+
+     this.aceEditor.getSession().setMode(LANG); //set language
      this.aceEditor.setShowFoldWidgets(true); // for the scope fold feature
 
      this.aceEditor.renderer.attachToShadowRoot()
+     this.consoleCode();
+     
   }
+
+  getEditorOptions(): Partial<ace.Ace.EditorOptions> & { enableBasicAutocompletion?: boolean; } {
+    const basicEditorOptions: Partial<ace.Ace.EditorOptions> = {
+        highlightActiveLine: true,
+        minLines: 14,
+        maxLines: Infinity,
+        
+    };
+
+    const extraEditorOptions = {
+        enableBasicAutocompletion: true,
+        // mode: 'groovy',
+      //  theme: 'textmate',
+       showGutter: true,     // Enabling line numbers for the editor
+       useWrapMode: true,
+       showPrintMargin: false, // Disabling print margin line from the editor
+      // showInvisibles: true,
+      highlightGutterLine: false,
+      highlightActiveLine: false,
+      fadeFoldWidgets: true,
+      showLineNumbers: true,
+      howGutter: true,
+      fontSize: 12,
+      wrap: false,
+
+    };
+    const margedOptions = Object.assign(basicEditorOptions, extraEditorOptions);
+    return margedOptions;
+}
+
+setCodeTutorial(){
+  this.aceEditor.session.insert({row: 1, column: 0}, "<h1>Cstomr Nam:</h1>\n");
+  this.aceEditor.session.insert({row: 1, column: 0}, "<h3>John Hopkins</h3>");
+  
+}
+
+refresh(){
+  this.setContent("");
+  this.setCodeTutorial();
+}
+
+beautifyContent() {
+  if (this.aceEditor && this.editorBeautify) {
+     const session = this.aceEditor.getSession();
+     this.editorBeautify.beautify(session);
+  }
+}
+
+
+getCode() {
+  const code = this.aceEditor.getValue();
+  console.log(code);
+}
+
+setContent(content: string): void {
+  if (this.aceEditor) {
+      this.aceEditor.setValue(content);
+  }
+}
+
+
+consoleCode() {
+  console.log(this.getContent());
+  this.htmltest = this.aceEditor.getValue();
+  console.log(this.htmltest)
+  var correctCustmomerName = "<h1>Customer Name:</h1>\n<h3>John Hopkins</h3>"
+  console.log(correctCustmomerName)
+  if(this.htmltest.includes(correctCustmomerName)){
+    console.log("correct")
+  }
+}
+
+getContent() {
+  if (this.aceEditor) {
+      const code = this.aceEditor.getValue();
+      return code;
+  }
+  return
+}
+
+OnContentChange(callback: (content: string, delta: ace.Ace.Delta) => void): void {
+  this.aceEditor.on('change', (delta) => {
+      const content = this.aceEditor.getValue();
+      callback(content, delta);
+  });
+}
 }
