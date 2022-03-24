b2b1ec22f343d050f57d5972cc0212f5d13f60fa
bartek.skorecki <59570853+Barto670@users.noreply.github.com>
2022-03-24 23:58:44 +0000

    -feeded data into statistics page

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -9,6 +9,12 @@ import { Router } from '@angular/router';
 export class StatisticsComponent implements OnInit {
 
   user: any;
+  incorrect! : number;
+  successPercent! : number;
+
+  tutorialProgress! : number;
+  basicsProgress! : number;
+  websitesProgress! : number;
 
   constructor(private router: Router) { }
 
@@ -16,8 +22,39 @@ export class StatisticsComponent implements OnInit {
     if(localStorage.getItem('user') == null){
       this.router.navigate(["/login"]);
     }
+    
 
     this.user = JSON.parse(localStorage.getItem('user')!); // The non-null assertion operator at the end of the line
+
+    this.incorrect = this.user.answered-this.user.correct;
+    this.successPercent = this.user.correct/this.user.answered*100;
+
+
+    if(this.user.stage > 3){
+      this.tutorialProgress = 3;
+    }else if(this.user.stage == 0){
+      this.tutorialProgress = 0;
+    }else{
+      this.tutorialProgress = this.user.sectionStage;
+    }
+
+    if(this.user.stage > 6){
+      this.basicsProgress = 3;
+    }else if(this.user.stage <= 3){
+      this.basicsProgress = 0;
+    }else{
+      this.basicsProgress = this.user.sectionStage;
+    }
+
+    if(this.user.stage > 10){
+      this.websitesProgress = 4;
+    }else if(this.user.stage <= 6){
+      this.websitesProgress = 0;
+    }else{
+      this.websitesProgress = this.user.sectionStage;
+    }
+
+
   }
 
 }
