14245b2ae677458098c2d6372b9dc99dc90daa36
bartek.skorecki <59570853+Barto670@users.noreply.github.com>
2022-03-25 21:44:08 +0000

    -updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { LoginComponent} from "./login/login.component"
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './global/navbar/navbar.component';
-import { MatIconModule} from '@angular/material/icon';
 import { QuizComponent } from './quiz/quiz.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { TheoryComponent } from './theory/theory.component';
 import { Login2Component } from './login2/login2.component';
 import { Home2Component } from './home2/home2.component';
 import { AppServices } from './app.service';
-import { DatePipe } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import { popup } from './profile/popup/popup.component';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button'; 
 import { chat } from './global/navbar/chat/chat.component'
 import { AboutComponent } from './about/about.component';
 import { AboutLoginComponent } from './aboutLogin/aboutLogin.component';
-// import { ContactUsComponent } from './contactus/contactus.component';
 
 @NgModule({
 imports: [
@@ -59,7 +60,6 @@ declarations: [
     chat,
     AboutComponent,
     AboutLoginComponent,
-    // ContactUsComponent,
     
 ],
 providers: [AppServices ,DatePipe,NavbarComponent],
